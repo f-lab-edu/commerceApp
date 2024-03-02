fe2b2044a5303a25de1735c63740ce2a0e46eb33
hyun <rlaalgus132@naver.com>
2024-03-03 06:14:10 +0900

    feature[#4]: 장마구니 목록 조회, 장바구니에 상품 추가, 장바구니 상품 삭제기능 usecase 생셩

---
+++ app/src/main/java/com/example/commerceapp/domain/usecases/user/cart/AddToCartUseCase.kt
@@ -0,0 +1,18 @@
+package com.example.commerceapp.domain.usecases.user.cart
+
+import com.example.commerceapp.domain.extension.mapToResultEntity
+import com.example.commerceapp.domain.model.common.ErrorHandler
+import com.example.commerceapp.domain.model.common.RequestParam
+import com.example.commerceapp.domain.model.common.ResultEntity
+import com.example.commerceapp.domain.repository.UserRepository
+import com.example.commerceapp.domain.usecases.base.BaseFlowUseCase
+import kotlinx.coroutines.flow.Flow
+
+data class AddToCartUseCase(
+    val repository: UserRepository,
+    private val loginExceptionHandler: ErrorHandler
+) : BaseFlowUseCase<String>() {
+    override suspend fun invoke(parameters: RequestParam): Flow<ResultEntity<String>> =
+        repository.addToCart(parameters).mapToResultEntity(loginExceptionHandler)
+
+}
