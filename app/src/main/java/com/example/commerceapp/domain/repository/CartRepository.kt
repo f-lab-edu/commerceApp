80917b2bdd6f20eb6fd71b3b220a09e03a72d139
hyun <rlaalgus132@naver.com>
2024-03-03 23:00:32 +0900

    fix : [피드백 반영] cart 디렉토리 생성하여 관련 클래스들 모아서 관리하도록 수정 & api응답 타입 Response로 변경
    요청에 대한 응답 타입이 변경될 수 있으므로 response타입으로 수정
    
    issue [#4]

--- app/src/main/java/com/example/commerceapp/domain/repository/CartRepository.kt
+++ app/src/main/java/com/example/commerceapp/domain/repository/CartRepository.kt
@@ -1,4 +1,12 @@
 package com.example.commerceapp.domain.repository
 
+import com.example.commerceapp.domain.model.common.RequestParam
+import com.example.commerceapp.domain.model.product.ProductPreview
+import com.example.commerceapp.domain.response.Response
+import kotlinx.coroutines.flow.Flow
+
 interface CartRepository {
+    fun getCartList(requestParam: RequestParam): Flow<List<ProductPreview>>
+    fun addToCart(requestParam: RequestParam): Flow<Response>
+    fun removeFromCart(requestParam: RequestParam): Flow<Response>
 }
---
+++ app/src/main/java/com/example/commerceapp/domain/usecases/cart/AddToCartUseCase.kt
@@ -0,0 +1,19 @@
+package com.example.commerceapp.domain.usecases.cart
+
+import com.example.commerceapp.domain.extension.mapToResultEntity
+import com.example.commerceapp.domain.model.common.ErrorHandler
+import com.example.commerceapp.domain.model.common.RequestParam
+import com.example.commerceapp.domain.model.common.ResultEntity
+import com.example.commerceapp.domain.repository.CartRepository
+import com.example.commerceapp.domain.response.Response
+import com.example.commerceapp.domain.usecases.base.BaseFlowUseCase
+import kotlinx.coroutines.flow.Flow
+
+data class AddToCartUseCase(
+    val repository: CartRepository,
+    private val loginExceptionHandler: ErrorHandler
+) : BaseFlowUseCase<Response>() {
+    override suspend fun invoke(parameters: RequestParam): Flow<ResultEntity<Response>> =
+        repository.addToCart(parameters).mapToResultEntity(loginExceptionHandler)
+
+}
