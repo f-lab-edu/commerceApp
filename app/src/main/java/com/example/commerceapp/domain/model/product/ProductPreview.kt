19df4793a0d9aec0d0edf7eabcc25e2698ba91e4
hyun <rlaalgus132@naver.com>
2024-04-14 19:11:34 +0900

    fix[#9] : Product, ProductDetail필드 변경
    상품 데이터를 단순한 데이터셋으로 변경하면서 불필요한 필드 삭제함.

--- app/src/main/java/com/example/commerceapp/domain/model/product/ProductPreview.kt
+++ app/src/main/java/com/example/commerceapp/domain/model/product/ProductPreview.kt
@@ -1,18 +1,15 @@
 package com.example.commerceapp.domain.model.product
 
-import com.example.commerceapp.domain.model.Brand
-
 data class ProductPreview(
-    val id: String? = null,
-    val averageReviewScore: Double = 0.0,
-    val brand: Brand? = null,
-    val images: List<String> = emptyList(),
-    val isBest: Boolean = false,
-    val isNewItem: Boolean = false,
-    val discountPrice: Int = 0,
-    val discountRate: Int = 0,
-    val name: String = "",
-    val productNo: String? = null,
-    val salePrice: Int = 0,
-    val bookMark:Boolean = false
+    val retailPrice: Int,
+    val basePrice: Int,
+    val discountedPrice: Int,
+    val discountRate: Double,
+    val expirationDate: String?,
+    val isSoldOut: Boolean,
+    val mainImageUrl: String,
+    val name: String,
+    val no: String,
+    val reviewCount: Int,
+    val adultVerificationFailed: Boolean,
 )
