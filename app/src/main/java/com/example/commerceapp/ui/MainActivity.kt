5fdc2b014d9b39fac46358f5a68a712b26910079
hyun <rlaalgus132@naver.com>
2024-07-08 04:03:52 +0900

    fix[#23] : [피드백 반영] BottomNavigationBar 그림자 효과 적용
    잘못 적용한 zIndex제거 및 Surface이용하여 그림자 효과 추가

--- app/src/main/java/com/example/commerceapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/commerceapp/ui/MainActivity.kt
@@ -7,10 +7,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,13 +20,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.zIndex
 import androidx.navigation.NavController
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import com.example.commerceapp.ui.theme.CommerceAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -91,25 +90,32 @@ fun BottomNavigationBar(
     items: List<BottomNavigationScreen>
 ) {
     val currentRoute = navController.currentDestination?.route ?: items.firstOrNull()
-    Row(
+    Surface(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(horizontal = 16.dp)
-            .zIndex(1f),
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically
+            .height(56.dp),
+        shadowElevation = 8.dp
     ) {
-        items.forEach {
-            NavigationBarItem(
-                selected = currentRoute == it.route,
-                onClick = { navController.navigate(it.route) },
-                icon = {
-                    Icon(
-                        painter = painterResource(id = it.iconResourceId),
-                        contentDescription = stringResource(id = it.stringResourceId)
-                    )
-                }
-            )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+                .background(Color.White),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            items.forEach {
+                NavigationBarItem(
+                    selected = currentRoute == it.route,
+                    onClick = { navController.navigate(it.route) },
+                    icon = {
+                        Icon(
+                            painter = painterResource(id = it.iconResourceId),
+                            contentDescription = stringResource(id = it.stringResourceId)
+                        )
+                    }
+                )
+            }
         }
     }
 }
