997cc29e9237474ed4ede1ef0ad07b45505c8662
hyun <rlaalgus132@naver.com>
2024-07-15 01:51:21 +0900

    feature[#30] : 새로 정의한 ResultEntity에 맞게 Product관련 핸들러 객체 수정

--- app/src/main/java/com/example/commerceapp/domain/model/product/ProductPreviewHandler.kt
+++ app/src/main/java/com/example/commerceapp/domain/model/product/ProductPreviewHandler.kt
@@ -1,14 +1,20 @@
 package com.example.commerceapp.domain.model.product
 
 import com.example.commerceapp.domain.extension.toErrorType
+import com.example.commerceapp.domain.model.common.DataError
+import com.example.commerceapp.domain.model.common.Error
 import com.example.commerceapp.domain.model.common.ErrorHandler
+import com.example.commerceapp.domain.model.common.InvalidDataException
 import com.example.commerceapp.domain.model.common.ResultEntity
 
 class ProductPreviewHandler : ErrorHandler {
-    override fun <ProductPreview> handle(throwable: Throwable): ResultEntity.Error<ProductPreview> {
+    override fun <ProductPreview, Error> handle(throwable: Throwable): ResultEntity.Error<ProductPreview, DataError> {
         return ResultEntity.Error(
-            error = throwable.toErrorType(),
-            message = throwable.message ?: throwable.stackTraceToString()
+            error = throwable.toErrorType()
         )
+    }
+
+    override fun <D, E> handle(throwable: InvalidDataException): ResultEntity<D, Error> {
+        TODO("Not yet implemented")
     }
 }
