1c930cd136f5b09096731b5f4844ea587db9f993
hyun <rlaalgus132@naver.com>
2024-07-25 18:01:43 +0900

    refactor[#33] : 로그인 및 회원가입 관련 객체에 ResultEntity 및 Error타입 변경사항 반영

--- app/src/main/java/com/example/commerceapp/domain/usecases/auth/LoginUseCase.kt
+++ app/src/main/java/com/example/commerceapp/domain/usecases/auth/LoginUseCase.kt
@@ -1,18 +1,18 @@
 package com.example.commerceapp.domain.usecases.auth
 
 import com.example.commerceapp.domain.extension.mapToResultEntity
+import com.example.commerceapp.domain.model.common.Error
 import com.example.commerceapp.domain.model.common.ErrorHandler
 import com.example.commerceapp.domain.model.common.RequestParam
 import com.example.commerceapp.domain.model.common.ResultEntity
 import com.example.commerceapp.domain.model.user.UserPreview
 import com.example.commerceapp.domain.repository.AuthRepository
-import com.example.commerceapp.domain.usecases.base.BaseFlowUseCase
 import kotlinx.coroutines.flow.Flow
 
 class LoginUseCase(
     private val repository: AuthRepository,
     private val loginExceptionHandler: ErrorHandler
-) : BaseFlowUseCase<UserPreview>() {
-    override suspend fun invoke(parameters: RequestParam): Flow<ResultEntity<UserPreview>> =
+) {
+    suspend fun invoke(parameters: RequestParam): Flow<ResultEntity<UserPreview,Error>> =
         repository.login(parameters).mapToResultEntity(loginExceptionHandler)
 }
