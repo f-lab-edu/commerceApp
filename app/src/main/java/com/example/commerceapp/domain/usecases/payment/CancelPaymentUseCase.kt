7be32cb29c127cae5f012c1430023f97f90ed015
hyun <rlaalgus132@naver.com>
2024-07-25 18:12:29 +0900

    refactor[#33] : 결제 관련 usecase에 ResultEntity 및 Error타입 변경사항 반영

--- app/src/main/java/com/example/commerceapp/domain/usecases/payment/CancelPaymentUseCase.kt
+++ app/src/main/java/com/example/commerceapp/domain/usecases/payment/CancelPaymentUseCase.kt
@@ -1,19 +1,19 @@
 package com.example.commerceapp.domain.usecases.payment
 
 import com.example.commerceapp.domain.extension.mapToResultEntity
+import com.example.commerceapp.domain.model.common.Error
 import com.example.commerceapp.domain.model.common.ErrorHandler
 import com.example.commerceapp.domain.model.common.RequestParam
 import com.example.commerceapp.domain.model.common.ResultEntity
 import com.example.commerceapp.domain.repository.PaymentRepository
 import com.example.commerceapp.domain.response.Response
-import com.example.commerceapp.domain.usecases.base.BaseFlowUseCase
 import kotlinx.coroutines.flow.Flow
 
 class CancelPaymentUseCase(
     private val repository: PaymentRepository,
     private val handler: ErrorHandler
-) : BaseFlowUseCase<Response>() {
-    override suspend fun invoke(parameters: RequestParam): Flow<ResultEntity<Response>> =
+) {
+    suspend fun invoke(parameters: RequestParam): Flow<ResultEntity<Response, Error>> =
         repository.cancelPayment(parameters).mapToResultEntity(handler)
 
 }
