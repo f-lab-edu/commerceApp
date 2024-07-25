c2b46ffb19bf2a45cef113edd2323e2a441c9b01
hyun <rlaalgus132@naver.com>
2024-07-25 18:10:22 +0900

    refactor[#33] : 상품 브랜드 및 카테고리 usecase에 ResultEntity 및 Error타입 변경사항 반영

--- app/src/main/java/com/example/commerceapp/domain/usecases/product/GetAllBrands.kt
+++ app/src/main/java/com/example/commerceapp/domain/usecases/product/GetAllBrands.kt
@@ -2,6 +2,7 @@ package com.example.commerceapp.domain.usecases.product
 
 import com.example.commerceapp.domain.extension.mapToResultEntity
 import com.example.commerceapp.domain.model.Brand
+import com.example.commerceapp.domain.model.common.Error
 import com.example.commerceapp.domain.model.common.ErrorHandler
 import com.example.commerceapp.domain.model.common.ResultEntity
 import com.example.commerceapp.domain.repository.ProductRepository
@@ -12,6 +13,6 @@ class GetAllBrands @Inject constructor(
     private val repository: ProductRepository,
     private val errorHandler: ErrorHandler
 ) {
-    suspend fun invoke(): Flow<ResultEntity<List<Brand>>> =
+    suspend fun invoke(): Flow<ResultEntity<List<Brand>, Error>> =
         repository.getAllBrand().mapToResultEntity(errorHandler)
 }
--- app/src/main/java/com/example/commerceapp/domain/usecases/product/GetAllCategories.kt
+++ app/src/main/java/com/example/commerceapp/domain/usecases/product/GetAllCategories.kt
@@ -2,6 +2,7 @@ package com.example.commerceapp.domain.usecases.product
 
 import com.example.commerceapp.domain.extension.mapToResultEntity
 import com.example.commerceapp.domain.model.Category
+import com.example.commerceapp.domain.model.common.Error
 import com.example.commerceapp.domain.model.common.ErrorHandler
 import com.example.commerceapp.domain.model.common.ResultEntity
 import com.example.commerceapp.domain.repository.ProductRepository
@@ -12,6 +13,6 @@ class GetAllCategories @Inject constructor(
     private val repository: ProductRepository,
     private val errorHandler: ErrorHandler
 ) {
-    suspend fun invoke(): Flow<ResultEntity<List<Category>>> =
+    suspend fun invoke(): Flow<ResultEntity<List<Category>, Error>> =
         repository.getAllCategory().mapToResultEntity(errorHandler)
 }
